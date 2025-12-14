0a4dc36cc2b253178cc03c7a8b3060884b1693c6
joe-qodes <jquao04@gmail.com>
2025-12-14 20:53:48 +0000

    Created MySQLServer.py file

---
+++ MySQLServer.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+my_db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Joequao241204???&"
+)
+
+
+try:
+    cursor = my_db.cursor()
+    create_db = cursor.execute("CREATE DATABASE alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+    cursor.close()
+    my_db.close()
+except:
+    raise FileExistsError("The database already exists")
+
+
+# my_db = mysql.connector.connect(
+#     host = "localhost",
+#     user = "root",
+#     password = "Joequao241204???&",
+#     database = "my_db"
+# )
